6282054c1b586ea271ff5dcea0c5d129d4a56e25
Indresh <indresh.lucky101@gmail.com>
2025-03-29 11:40:41 +0530

    render db updated

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -1,6 +1,9 @@
 
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
+import dj_database_url
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,15 +76,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'student',
-        'USER': 'indresh',
-        'PASSWORD': 'indresh',
-        'HOST': 'localhost',  
-        'PORT': '5432',  
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
